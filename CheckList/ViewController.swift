2ae6b479f3cdbeb3914a006ffbf28ead9bd08a22
Lapua <c011731950@edu.teu.ac.jp>
2019-11-30 17:16:10 +0900

    first commit

---
+++ CheckList/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CheckList
+//
+//  Created by CrystalArrow on 2019/11/28.
+//  Copyright © 2019 CrystalArrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
